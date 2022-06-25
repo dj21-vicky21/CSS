1200a2799fab37ba1b97eaa1dc991949ce66b5e7
dj21-vicky21 <djvicky141@gmail.com>
2022-06-25 10:08:08 +0530

    js

--- task1/task1.js
+++ task1/task1.js
@@ -21,47 +21,56 @@ function number1(){
 }
 
 function number2_3(){
-    start = 1
-    end   = 100
-    count = 0
+    
     let divisor2 = document.getElementById('number2')
     let divisor3 = document.getElementById('number3')
     let output2  = document.getElementById('span2')
     let count2  = document.getElementById('count2')
-    output2.innerHTML = ""
-    while (start < end){
-        if (start%divisor2.value == 0 && start%divisor3.value == 0) {
-            output2.innerHTML += " "+start
-            count +=1
-        }
-        start += 1
-    }
-    count2.innerHTML = 'count: '+count
-    if (output2.innerHTML == '' ||  divisor2 > 100 || divisor3 >100 ){
-        output2.innerHTML = 'Input must be a Number and  Number < 100'
-    }
     
+    if (output2.innerHTML != '' ||  divisor2.value < 100 && divisor3.value < 100 ){
+        start = 1
+        end   = 100
+        count = 0
+        output2.innerHTML = ""
+        while (start < end){
+            if (start%divisor2.value == 0 && start%divisor3.value == 0) {
+                output2.innerHTML += " "+start
+                count +=1
+            }
+            start += 1
+        }
+        count2.innerHTML = 'count: '+count
+    }
+    else{
+        output2.innerHTML = 'Input must be a Number and  Number < 100'
+        count.innerText = 'Count: 0'
+
+    }    
     
 }
 
 function number4_5(){
-    start = 1
-    end   = 100
-    count = 0
     let divisor4 = document.getElementById('number4')
     let divisor5 = document.getElementById('number5')
     let output3  = document.getElementById('span3')
     let count3  = document.getElementById('count3')
-    output3.innerHTML = " "
-    while (start < end){
-        if (start%divisor4.value == 0 || start%divisor5.value == 0) {
-            output3.innerHTML += " "+start
-            count +=1
-        }
-        start += 1
-    } 
-    count3.innerHTML = 'count: '+count
-    if (output3.innerHTML == '' ||  divisor4 > 100  || divisor5 > 100 ){
+    if (output3.innerHTML != '' ||  divisor4.value  < 100  && divisor5.value < 100 ){
+        start = 1
+        end   = 100
+        count = 0
+        output3.innerHTML = " "
+        while (start < end){
+            if (start%divisor4.value == 0 || start%divisor5.value == 0) {
+                output3.innerHTML += " "+start
+                count +=1
+            }
+            start += 1
+        } 
+        count3.innerHTML = 'count: '+count
+    }
+    else{
         output3.innerHTML = 'Input must be a Number and  Number < 100'
+        count3.innerText = 'count: 0' 
+
     }
 }
