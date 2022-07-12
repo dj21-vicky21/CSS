16a2f1cee3c47d082eb605499cf2ae516dc762c4
dj21-vicky21 <djvicky141@gmail.com>
2022-07-12 19:42:06 +0530

    give style

---
+++ jsfunction/login_link_in_db/script.js
@@ -0,0 +1,6 @@
+let signup =()=>{
+    let page1 = document.querySelector('.loginpage')
+    let page2 = document.querySelector('.signuppage')
+    page1.style.display = 'none'
+    page2.style.display = 'block'
+}
