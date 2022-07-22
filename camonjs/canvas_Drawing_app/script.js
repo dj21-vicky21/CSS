2870bb36a2d2ac42f6be6a74b238bb0f29963436
dj21-vicky21 <djvicky141@gmail.com>
2022-07-22 15:16:09 +0530

    img-filter-app 50% and drawing-app 30% and curser small modification

--- camonjs/canvas_Drawing_app/script.js
+++ camonjs/canvas_Drawing_app/script.js
@@ -2,14 +2,14 @@ function run(){
     
 
     var width = window.innerWidth;
-        var height = window.innerHeight/1.43;
-        var layer = new Konva.Layer();
+    var height = window.innerHeight/2
+    var layer = new Konva.Layer();
     var stage = new Konva.Stage({
         container: 'container',
         width: width,
         height: height,
       });    
-      
+    
     square.addEventListener('click',()=>{
         var sq1 = new Konva.Rect({
           x: 20,
@@ -40,9 +40,24 @@ function run(){
           strokeWidth: 4,
           draggable:true
         });
+        
         // add the shape to the layer
         layer.add(rect1);
         stage.add(layer)
+    })
+    triangle.addEventListener('click',()=>{
+      var triangle = new Konva.RegularPolygon({
+        x: 100,
+        y: 100,
+        sides: 3,
+        radius: 80,
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable:true
+      });
+      layer.add(triangle);
+      stage.add(layer);
     })
     circle.addEventListener('click',()=>{
         var circle = new Konva.Circle({
@@ -61,10 +76,28 @@ function run(){
           // add the layer to the stage
           stage.add(layer);
     })
+    ring.addEventListener('click',()=>{
+      var ring = new Konva.Ring({
+        x: 100,
+        y: 100,
+        innerRadius: 40,
+        outerRadius: 70,
+        fill: 'yellow',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable:true
+      });
+
+      // add the shape to the layer
+      layer.add(ring);
+
+      // add the layer to the stage
+      stage.add(layer);
+    })
     star.addEventListener('click',()=>{
         var star = new Konva.Star({
-            x: stage.width() / 2,
-            y: stage.height() / 2,
+            x: 100,
+            y: 100,
             numPoints: 6,
             innerRadius: 40,
             outerRadius: 70,
@@ -83,7 +116,7 @@ function run(){
     polygon.addEventListener('click',()=>{
         var hexagon = new Konva.RegularPolygon({
             x: 100,
-            y: 150,
+            y: 100,
             sides: 6,
             radius: 70,
             fill: 'red',
--- camonjs/img_filter2.0/script.js
+++ camonjs/img_filter2.0/script.js
@@ -1,65 +1,113 @@
 function run(){
+    function loadImages(sources, callback) {
+        var images = {};
+        var loadedImages = 0;
+        var numImages = 0;
+        for (var src in sources) {
+          numImages++;
+        }
+        for (var src in sources) {
+          images[src] = new Image();
+          images[src].onload = function () {
+            if (++loadedImages >= numImages) {
+              callback(images);
+            }
+          };
+          images[src].src = sources[src];
+        }
+      }
+      function buildStage(images) {
+        var stage = new Konva.Stage({
+          container: 'container',
+          width: window.innerWidth/2,
+          height: window.innerHeight,
+        });
+
+        var layer = new Konva.Layer();
+
+        var pic = new Konva.Image({
+          image: images.pic,
+          x: 0,
+          y:0,
+          width:window.innerWidth/2,
+          height:window.innerHeight,
+        //   draggable: true,
+        });
+      
+
+
+        // Filter section start
+        pic.cache();      
+        layer.add(pic);
+        stage.add(layer);
+        
+        pic.position({
+          x: 0,
+          y: 0,
+        });
+        pic.cache();
+        pic.filters([Konva.Filters.Brighten,Konva.Filters.Contrast,Konva.Filters.Noise,Konva.Filters.Pixelate,Konva.Filters.HSL,Konva.Filters.HSV,Konva.Filters.Blur,Konva.Filters.Enhance]);
+        layer.add(pic);
+        stage.add(layer);
+        
+        var sliders = ['brightness','contrast','noise','pixelSize','hue','saturation','luminance','enhance','value'];
+        sliders.forEach(function (attr) {
+            var slider = document.getElementById(attr);
+            function update() {
+    
+            pic[attr](parseFloat(slider.value));
+            console.log(slider.value);
+          }
+          slider.oninput = update;
+  
+          update();
+
+        
+        });
     
 
-    // brightness.oninput = function() {
-    //     console.log(this.value);
-    // }
-    // program-starts
-    // required
-    const canvas =document.getElementById('canvas');
-    if(canvas.getContext){
-        const ctx = canvas.getContext('2d')
-        let img = new Image();
-        let filename = '';
-        const upload_btn = document.getElementById('selectedFile') 
 
-        upload_btn.addEventListener('change',(e)=>{
+        var blur = document.getElementById('blur');
+        blur.oninput = function () {
+          pic.blurRadius(blur.value);
+        };
+
+
+        // var kaleidoscope = document.getElementById('kaleidoscope');
+        // lion.kaleidoscopePower(3);
+        // kaleidoscope.oninput = function () {
+        //   lion.kaleidoscopeAngle(kaleidoscope.value);
+        // };
+
+
+// Filter section end
+// Remove Filter
+        
+      }
+
+      var img 
+      let upload_btn = document.getElementById('selectedFile')
+      // display img
+      upload_btn.addEventListener('change',(e)=>{
             const file = document.getElementById('selectedFile').files[0];
             const reader = new FileReader();
             if(file){
                 filename = file.name;
                 reader.readAsDataURL(file);
             }
-            // display img start
             reader.addEventListener('load',()=>{
                 img = new Image();
                 img.src = reader.result;
-                img.onload = function(){
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    ctx.drawImage(img,0,0,img.width,img.height)
-                }
-    
-            });
-            // display img end
-        });
+                var sources = {
+                  pic: img.src,
+                };
 
-        var brightness = document.getElementById('brightness-value')
-        var contrast = document.getElementById('contrast-value')
-        var contrast = document.getElementById('contrast-value')
-        brightness.oninput = function(){
-                console.log(this.value);
-                if(this.value > 0){
-                    Caman("#canvas",img,function(){
-                        this.brightness(1).render()
-                    });
-                }
-                else if(this.value < 0){
-                    Caman("#canvas",img,function(){
-                        this.brightness(-1).render()
-                    });
-                }
-                
-            }
-        // contrast.oninput = function(){
-        //         console.log(this.value);
-        //     }
-        
-       
-        
-            
-    
-    
-    }
-    
+                loadImages(sources, buildStage);
+  
+            });
+      });    
+
+
 }
+
+
